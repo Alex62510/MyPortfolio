3e9d21f862982bdfcc742b49fd277852acc71b7c
Alex62510 <eagle625@tut.by>
2023-05-31 23:26:56 +0300

    commit 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Skills from "./skils/Skills";
+import RemoteWork from "./remoteWork/RemoteWork";
+import Contact from "./Contacts/Contact";
+import Projects from "./Projects/Projects";
+import Footer from "./Footer/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <Projects/>
+            <RemoteWork/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
 }
-
 export default App;
