6b3f79e9ab153f522063980417565360d4b10fc3
Alex62510 <eagle625@tut.by>
2023-06-25 21:58:58 +0300

    commit 031

--- src/Contacts/Contact.tsx
+++ src/Contacts/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './Contact.module.css';
+import style from './Contact.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
 import Title from "../common/components/title/Title";
 
@@ -7,12 +7,12 @@ const Contact = () => {
     return (
         <div className={style.contacts}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <Title title="Contakts"/>
+                <Title title="Contact Me"/>
                 <form className={style.contactForm}>
-                    <input className={style.input}/>
-                    <input className={style.input}/>
-                    <textarea className={style.textarea}></textarea>
-                    <button type='submit' className={style.submitButton}>SEND</button>
+                    <input className={style.input} placeholder={"Name*"}/>
+                    <input className={style.input} placeholder={"Email*"}/>
+                    <textarea className={style.textarea} placeholder={"Your massage*"}></textarea>
+                    <button type='submit' className={style.submitButton}>Send message</button>
                 </form>
             </div>
         </div>
