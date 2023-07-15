8e035d76f3f67264b631d9aeff636a2da3c02fbc
Alex62510 <eagle625@tut.by>
2023-07-15 23:50:37 +0300

    commit 050

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './Header.module.scss';
-import Nav from "../Nav/Nav";
+import Nav from "./Nav/Nav";
+import BurgerNav from "./burgerNav/burgerNav";
+
 const Header = () => {
     return (
         <div className={style.header}>
-           <Nav/>
+            <Nav/>
+            <BurgerNav/>
         </div>
     );
 };
---
+++ src/Header/burgerNav/burgerNav.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import style from './burgerNav.module.scss';
+import {Link} from "react-scroll";
+
+const BurgerNav = () => {
+    return (
+        <div className={style.burgerNav}>
+            <div className={style.burgerNavItems}>
+                <Link to="main"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      offset={-90}
+                >main</Link>
+                <Link to="skills"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      offset={1}
+                >skills</Link>
+                <Link to={"projects"}
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      offset={1}
+                >projects</Link>
+                <Link to="contacts"
+                      activeClass={style.active}
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                      offset={60}
+                >contacts</Link>
+            </div>
+            <div className={style.burgerBtn}></div>
+        </div>
+    );
+};
+
+export default BurgerNav;
