d95106d9e0e8c7ae12a12dbd9e9b27c9f6e456f1
Alex62510 <eagle625@tut.by>
2023-07-17 10:43:40 +0300

    commit 053

--- src/Header/burgerNav/burgerNav.tsx
+++ src/Header/burgerNav/burgerNav.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './burgerNav.module.scss';
 import {Link} from "react-scroll";
 
 const BurgerNav = () => {
+    const [bMenuOn, setBMenuOn] = useState<boolean>(false)
+    const bMenuOnClick = () => {
+        setBMenuOn(!bMenuOn)
+    }
+
     return (
         <div className={style.burgerNav}>
-            <div className={style.burgerNavItems}>
+            <div className={bMenuOn ? `${style.burgerNavItems} ${style.show}` : style.burgerNavItems}>
                 <Link to="main"
                       activeClass={style.active}
                       spy={true}
@@ -35,7 +40,7 @@ const BurgerNav = () => {
                       offset={60}
                 >contacts</Link>
             </div>
-            <div className={style.burgerBtn}></div>
+            <div className={style.burgerBtn} onClick={bMenuOnClick}></div>
         </div>
     );
 };
