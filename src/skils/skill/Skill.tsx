0d6b2a67dfd5c86a8e99cd28a8f70e9d15ecaa36
Alex62510 <eagle625@tut.by>
2023-09-17 21:25:49 +0300

    commit 04 add project FleshCard

--- src/skils/skill/Skill.tsx
+++ src/skils/skill/Skill.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import style from './Skill.module.scss';
-import {Simulate} from "react-dom/test-utils";
-import progress = Simulate.progress;
 
 type SkillPropsType ={
     title:string
