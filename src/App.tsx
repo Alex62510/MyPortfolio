f1e24ef8cf6b0db117ae11677983f6e133ca2e54
Alex62510 <eagle625@tut.by>
2024-05-22 22:07:17 +0300

    commit 051 add chat

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Particle from "./common/components/particle/Particle";
 function App() {
     const [blogLoading, setBlogLoading] = useState<boolean>(false)
     const [estateLoading, setEstateLoading] = useState<boolean>(false)
+    const [chatLoading, setChatLoading] = useState<boolean>(false)
 
     useEffect(() => {
         const getBlogApp = async () => {
@@ -46,22 +47,37 @@ function App() {
                 setEstateLoading(false);
             }
         };
+        const getChatApp = async () => {
+            setChatLoading(true);
+            try {
+                const res = await fetch('https://chat-mern-kus4.onrender.com/',{mode:"no-cors"});
+                const data = await res.json();
+                if (res.ok) {
+                    setChatLoading(false);
+                } else {
+                    setChatLoading(false);
+                    console.log(data.error);
+                }
+            } catch (e) {
+                console.log(e);
+                setChatLoading(false);
+            }
+        };
         getBlogApp();
         getEstateApp()
+        getChatApp()
     }, []);
 
     return (
         <div className="App">
-
             <Particle/>
             <Header/>
             <Main/>
             <Skills/>
-            <Projects blogLoading={blogLoading} estateLoading={estateLoading}/>
+            <Projects blogLoading={blogLoading} estateLoading={estateLoading} chatLoading={chatLoading}/>
             <RemoteWork/>
             <Contact/>
             <Footer/>
-
         </div>
     );
 }
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -7,17 +7,19 @@ import SocialNetImg from '../assets/image/Social-network.png'
 import fleshCardImg from '../assets/image/FleshCardImg.png'
 import musicImg from '../assets/image/MusicImg.jpg'
 import algoImg from '../assets/image/AlgoImg.jpg'
-import chatImg from '../assets/image/ChatImg.jpg'
+import chatImg from '../assets/image/groupChatImg.jpg'
 import estateImg from '../assets/image/estateImg.jpg'
 import blogImg from '../assets/image/blogImg.jpg'
+import groupChatImg from '../assets/image/ChatImg.jpg'
 import {Zoom} from "react-awesome-reveal";
 
 
 type Props = {
     blogLoading: boolean
     estateLoading: boolean
+    chatLoading:boolean
 }
-const Projects = ({blogLoading, estateLoading}: Props) => {
+const Projects = ({blogLoading, estateLoading,chatLoading}: Props) => {
     const todos = {
         backgroundImage: `url(${todoImg})`,
     };
@@ -41,6 +43,9 @@ const Projects = ({blogLoading, estateLoading}: Props) => {
     };
     const blog = {
         backgroundImage: `url(${blogImg})`,
+    };
+    const groupChat = {
+        backgroundImage: `url(${groupChatImg})`,
     };
 
     return (
@@ -77,18 +82,23 @@ const Projects = ({blogLoading, estateLoading}: Props) => {
                     <Zoom>
                         <Project
                             link={"https://aesthetic-sfogliatella-600f15.netlify.app/"}
-                            title={"Chat"} style={chat}
-                            description={"This is an application for chatting"}/>
+                            title={"Group Chat"} style={groupChat}
+                            description={"This is an application for group communication"}/>
                     </Zoom>
                     <Zoom>
                         <Project link={"https://alex-orlov-estate-app.onrender.com/"}
                                  title={"Estate App"} style={estate} loading={estateLoading}
-                                 description={"This is a full-stack application for selling or renting an accommodation."}/>
+                                 description={"This is a full-stack application for selling or renting an accommodation. This application was created using TypeScript, NodeJs, Express, MongoDb, Tailwind"}/>
                     </Zoom>
                     <Zoom>
                         <Project link={"https://blog-fullstack-v4zf.onrender.com/"}
-                                 title={"Blog App"} style={blog} loading={blogLoading}
-                                 description={"This is a full-stack application for creating a personal blog."}/>
+                                 title={"My Blog"} style={blog} loading={blogLoading}
+                                 description={"This is a full-stack application for creating a personal blog. This application was created using TypeScript, NodeJs, Express, MongoDb, Tailwind"}/>
+                    </Zoom>
+                    <Zoom>
+                        <Project link={"https://chat-mern-kus4.onrender.com/"}
+                                 title={"Chat"} style={chat} loading={chatLoading}
+                                 description={"This is a full-stack application for chatting using WebSocket, TypeScript, NodeJs, Express, MongoDb, Tailwind."}/>
                     </Zoom>
                 </div>
             </div>
